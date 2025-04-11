ffa56752928e87782f895c70f850d5f5064c5624
Armando Andrés Meabe <armando.meabe@outlook.com>
2025-04-11 00:55:30 -0300

    introducing ollama

--- Cognitiva.AI.Simpler/Cognitiva.AI.Simpler.WinClient/MainForm.cs
+++ Cognitiva.AI.Simpler/Cognitiva.AI.Simpler.WinClient/MainForm.cs
@@ -1,10 +1,17 @@
 using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using OllamaClient;
+using Microsoft.Extensions.DependencyInjection;
+using OllamaClient.Extensions;
 
 namespace Cognitiva.AI.Simpler.WinClient
 {
-
     public partial class MainForm : Form
     {
+        private readonly OllamaHttpClient _ollamaService;
+
         public MainForm()
         {
             InitializeComponent();
@@ -23,6 +30,47 @@ namespace Cognitiva.AI.Simpler.WinClient
         private void button1_Click(object sender, EventArgs e)
         {
             this.webView21.Source = new Uri(this.textBox1.Text);
+
+            var serviceProvider = new ServiceCollection()
+                  .AddOllamaClient()
+                  .BuildServiceProvider();
+
+            var client = serviceProvider.GetRequiredService<IOllamaHttpClient>();
+
+            var pullResult = client.Pull(new OllamaClient.Models.PullRequest()
+            {
+                Name = "phi3",
+            }, CancellationToken.None);
+
+            Console.WriteLine(pullResult.Status);
+
+            var models = (client.GetModels(CancellationToken.None)).Result;
+
+            foreach (var model in models.Models)
+            {
+                Console.WriteLine(model.Name);
+            }
+
+            var result = client.SendChat(new OllamaClient.Models.ChatStreamRequest()
+            {
+                Model = models.Models[0].Name,
+                Messages = new List<OllamaClient.Models.Message>() { new OllamaClient.Models.Message() {
+                        Content = "Hello, how are you?",
+                        Role = "user"
+                    }
+                }
+            }, CancellationToken.None);
+
+            foreach (var message in result)
+            {
+                if (message.Message is null) continue;
+
+                Console.Write(message.Message.Content);
+            }
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
         }
     }
 }
